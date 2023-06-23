1ae59b90e46c685b8905cfb2f1973d8eaeffa6dd
TPriince <tobiloba110@gmail.com>
2023-06-23 12:56:03 +0100

    feat: Work on responsiveness for error page

--- src/views/ErrorPageView.vue
+++ src/views/ErrorPageView.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="mainbox">
-        <span class="err">4</span>
-        <i class="far fa-question-circle fa-spin"></i>
-        <span class="err2">4</span>
+        <div>
+            <span class="err">4</span>
+            <i class="far fa-question-circle fa-spin"></i>
+            <span class="err2">4</span>
+        </div>
         <div class="msg">Sorry, but we're having a hard time finding what you're searching for. Maybe try a different path?
             <p>Let's go <RouterLink to="/">home</RouterLink> and try from there.</p>
         </div>
@@ -24,43 +26,27 @@ export default defineComponent({
 
 <style scoped>
 .mainbox {
+    max-width: 600px;
+    height: 100vh;
     margin: auto;
-    width: 600px;
-    height: 600px;
-    position: relative;
+    text-align: center;
+    padding: 1.5rem;
 }
 
-.err {
-    color: var(--dark-blue);
-    font-size: 10rem;
-    position: absolute;
-    left: 20%;
-    top: 8%;
-}
-
-.far {
-    position: absolute;
-    font-size: 8.5rem;
-    left: 42%;
-    top: 16%;
-    color: var(--whitish);
-}
-
+.err,
 .err2 {
     color: var(--dark-blue);
     font-size: 10rem;
-    position: absolute;
-    left: 68%;
-    top: 8%;
+}
+
+.far {
+    font-size: 8.5rem;
+    color: var(--whitish);
 }
 
 .msg {
     text-align: center;
     font-size: 1.2rem;
-    position: absolute;
-    left: 16%;
-    top: 45%;
-    width: 75%;
     font-weight: var(--font-medium);
 }
 
@@ -72,5 +58,55 @@ a {
 
 a:hover {
     text-decoration: underline;
+}
+
+@media screen and (max-width: 600px) {
+
+    .err,
+    .err2 {
+        font-size: 6rem;
+    }
+
+    .far {
+        font-size: 5.1rem;
+    }
+
+    .msg {
+        font-size: 0.9rem;
+        padding: 0 2rem;
+    }
+}
+
+@media screen and (max-width: 320px) {
+
+    .err,
+    .err2 {
+        font-size: 5rem;
+    }
+
+    .far {
+        font-size: 4.1rem;
+    }
+
+    .msg {
+        font-size: 0.8rem;
+        padding: 0rem;
+    }
+}
+
+@media screen and (max-width: 280px) {
+
+    .err,
+    .err2 {
+        font-size: 3.5rem;
+    }
+
+    .far {
+        font-size: 2.8rem;
+    }
+
+    .msg {
+        font-size: 0.6rem;
+    }
 }
 </style>
